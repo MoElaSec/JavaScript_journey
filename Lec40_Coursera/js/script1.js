dad2a5e9b98b4a0dc1fe5de7cda2778ef7df0ee7
MoElaSec <mohd.debrecen@gmail.com>
2021-06-15 17:31:39 +0400

    Week-4 is done JS intro 🙌😍

--- Lec40_Coursera/js/script1.js
+++ Lec40_Coursera/js/script1.js
@@ -3,8 +3,20 @@
 //     console.log("Hello, " + name);
 // }
 
-var shifraGretter = {};
-shifraGretter.name = "shifraSec";
-shifraGretter.sayHello = function () {
-    console.log("Hello, " + shifraGretter.name);
-};
+// var shifraGretter = {};
+// shifraGretter.name = "shifraSec";
+// shifraGretter.sayHello = function () {
+//     console.log("Hello, " + shifraGretter.name);
+// };
+
+//we can achieve the same thing using IIFE
+(function (window) {
+    var shifraGretter = {};
+    shifraGretter.name = "shifraSec";
+    var greeting = "Hello, " //? notice we didn't have to use a fake namespace for the greeting as it's will be IIFE
+    shifraGretter.sayHello = function () {
+        console.log(greeting + shifraGretter.name);
+    };
+
+    window.shifraGretter = shifraGretter; // we exposed @shifraGretter to the global window
+})(window);
--- Lec40_Coursera/js/script2.js
+++ Lec40_Coursera/js/script2.js
@@ -4,8 +4,20 @@
 // }
 
 //let's fake a name space
-var moGretter = {}; //empty obj works as a namespace
-moGretter.name = "Mo";
-moGretter.sayHi = function () {
-    console.log("Hi " + moGretter.name);
-};
+// var moGretter = {}; //empty obj works as a namespace
+// moGretter.name = "Mo";
+// moGretter.sayHi = function () {
+//     console.log("Hi " + moGretter.name);
+// };
+
+//Let's use IIFE
+(function (window) {
+    var moGretter = {};
+    moGretter.name = "Mo";
+    var greeting = "Hi"; //? Won't collide with the other greeting cuz each is being defined in their own execution context = inside th IIFE
+    moGretter.sayHi = function () {
+        console.log(greeting + moGretter.name);
+    };
+
+    window.moGretter = moGretter;
+})(window);
