f3eb2d0aa6bb496745d0f77ba7ec792ed0c23bc8
MoElaSec <mohd.debrecen@gmail.com>
2021-06-15 14:37:07 +0400

    Lec_48 Ended 🙌

--- Lec40_Coursera/js/script.js
+++ Lec40_Coursera/js/script.js
@@ -1,6 +1,10 @@
 function Circle(radius) {
     this.radius = radius;
 }
+Circle.prototype.getArea = function() {
+    return Math.PI * this.radius ** 2;
+}
 
 var myCircle = new Circle(10);
+// console.log(myCircle.getArea());
 console.log(myCircle);
